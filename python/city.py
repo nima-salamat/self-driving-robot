94c9238f37423dd94e77eab80226ef383cff582c
nima <ssu87556@gmail.com>
2026-01-25 20:47:16 +0330

    handle json decodeError

--- python/city.py
+++ python/city.py
@@ -4,12 +4,15 @@ base_config.MODE="city"
 base_config.CONFIG_MODULE = config_city
 import os
 import json
-if config_city.CHANGE_WITH_JSON:
-    if os.path.exists("city.json"):
-        with open("city.json", "r") as f:
-            configs = json.loads(f.read())
-            for conf_name, value in configs.items():
-                setattr(config_city, conf_name, value)
+try:
+    if config_city.CHANGE_WITH_JSON:
+        if os.path.exists("city.json"):
+            with open("city.json", "r") as f:
+                configs = json.loads(f.read())
+                for conf_name, value in configs.items():
+                    setattr(config_city, conf_name, value)
+except json.JSONDecodeError:
+    pass
 from vision.camera import Camera
 from vision.city_vision_processing import VisionProcessor
 from vision.apriltag import ApriltagDetector
