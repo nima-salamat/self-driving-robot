3e3285e44a9a32dbb61a6df5ecf9311f1e26e8d9
nima <ssu87556@gmail.com>
2026-06-17 19:36:47 +0330

    resolve circular imports in mode configuration

--- python/utils/config_mode.py
+++ python/utils/config_mode.py
@@ -1,13 +1,15 @@
+# config_mode.py
+
 import base_config
-import modes.city.config_city as config_city 
-import modes.race.config_race as config_race
-
-
 
 def set_city_mode():
-    base_config.MODE="city"
+    import modes.city.config_city as config_city
+
+    base_config.MODE = "city"
     base_config.CONFIG_MODULE = config_city
 
 def set_race_mode():
-    base_config.MODE="race"
+    import modes.race.config_race as config_race
+
+    base_config.MODE = "race"
     base_config.CONFIG_MODULE = config_race
