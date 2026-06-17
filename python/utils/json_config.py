faf8db192d809cd02430056ce188e4441d67477c
nima <ssu87556@gmail.com>
2026-06-17 16:47:27 +0330

    sperate load config from json

---
+++ python/utils/json_config.py
@@ -0,0 +1,16 @@
+import os
+import json
+import config_city
+import base_config
+
+def load():
+    if base_config.MODE == "city":
+        try:
+            if config_city.CHANGE_WITH_JSON:
+                if os.path.exists("city.json"):
+                    with open("city.json", "r") as f:
+                        configs = json.loads(f.read())
+                        for conf_name, value in configs.items():
+                            setattr(config_city, conf_name, value)
+        except json.JSONDecodeError:
+            pass
