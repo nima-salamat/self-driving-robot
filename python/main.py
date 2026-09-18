7df568b70b8b36baaa64a667c6a616330a1be2d3
nima <152262921+nima-salamat@users.noreply.github.com>
2026-09-19 02:39:51 +0330

    Wire strict Arduino hardware contract into startup

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import signal
+import sys
 import threading
 
 from utils.parser import parse_args
@@ -8,6 +9,7 @@ from utils.preflight import run_preflight
 from utils.logging_setup import configure_logging
 from utils.runtime_metrics import RuntimeMetrics
 from utils.health import HealthMonitor
+from arduino.hardware_contract import ArduinoHardwareConfigError
 import base_config
 
 
@@ -40,6 +42,8 @@ if __name__ == "__main__":
     config.PERFORMANCE = args.performance
     config.WITHOUT_ARDUINO = args.without_arduino
     config.READ_ARDUINO_OUTPUT = args.read_arduino_output
+    config.ARDUINO_CONFIG = args.arduino_config
+    config.ARDUINO_CONTRACT_TIMEOUT = args.arduino_contract_timeout
     config.STREAM_ALLOW_CONTROL = args.stream_control
     config.USE_ML_LANE_DETECTOR = args.ml_lane_model is not None
     if args.ml_lane_model is not None:
@@ -68,4 +72,8 @@ if __name__ == "__main__":
     base_config.SHUTDOWN_EVENT = shutdown_event
     _install_shutdown_handlers(shutdown_event)
 
-    start()
+    try:
+        start()
+    except ArduinoHardwareConfigError as exc:
+        print(f"[ARDUINO HARDWARE CONTRACT ERROR] {exc}", file=sys.stderr, flush=True)
+        raise SystemExit(2)
